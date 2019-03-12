d3aa5b65c2f65b30ed8107765729d8b43875a711
Kijun-Park <ekdlwns3685@naver.com>
2019-03-12 20:28:34 +0900

    created detail query

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import { HashRouter as Router, Route } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import client from "./apolloClient";
 import Home from "./Home";
-import detail from "./Detail";
+import Detail from "./Detail";
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
         <Router>
-          <React.Fragment>
+          <main>
             <Route exact={true} path={"/"} component={Home} />
-            <Route path={"/details/:movieId"} component={Home} />
-          </React.Fragment>
+            <Route path={"/details/:movieId"} component={Detail} />
+          </main>
         </Router>
       </ApolloProvider>
     );
--- src/Detail.js
+++ src/Detail.js
@@ -1,5 +1,27 @@
 import React from "react";
+import { withRouter } from "react-router";
+import { Query } from "react-apollo";
+import { MOVIE_DETAILS } from "./queries";
 
-const detail = () => "Detail";
+const Detail = ({
+  match: {
+    params: { movieId }
+  }
+}) => (
+  <Query query={MOVIE_DETAILS} variables={{ movieId: parseInt(movieId) }}>
+    {({ loading, error, data }) => {
+      if (loading) return "loading";
+      if (error) return "error";
+      return (
+        <React.Fragment>
+          {data.movie.title}
+          {data.movie.medium_cover_image}
+          {data.movie.rating}
+          {data.movie.description_intro}
+        </React.Fragment>
+      );
+    }}
+  </Query>
+);
 
-export default detail;
+export default Detail;
